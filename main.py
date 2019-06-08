eb8ddb0e56641b51d1ea29023a7d068fa09f0adb
Willian <williann.nasc@gmail.com>
2019-06-08 18:08:45 -0300

    Update

--- main.py
+++ main.py
@@ -1,47 +1,128 @@
+from parse import *
 import requests
 import json
+import re
+
 
 with open('data.json', 'r') as f:
 	data = json.load(f)
 
-kimetsu_id = str(data['kimetsu'])
-kimetsu = requests.head(f'https://www.dreamanimes.com.br/online/legendado/kimetsu-no-yaiba/episodio/{kimetsu_id}')
 
-opm_id = str(data['opm'])
-if int(opm_id) < 10:
-	opm = requests.head(f'https://myanimesonline.net/episodios/one-punch-man-2-episodio-0{opm_id}/')
-else:
-	opm = requests.head(f'https://myanimesonline.net/episodios/one-punch-man-2-episodio-{opm_id}/')
-
-dororo_id = str(data['dororo'])
-dororo = requests.head(f'https://ww4.animesonline.online/video/dororo-episodio-{dororo_id}/')
+class Commands(Parse):
+	def __init__(self):
+		super().__init__(Commands)
 
 
-if kimetsu.status_code == 200:
-	print(f'Saiu ep novo de Kimetsu no Yaiba! EP {kimetsu_id}.')
-	data['kimetsu'] += 1
+	@set_root
+	def _root():
+		return {
+		'version': 'Version 1.0.0',
+		'greeting': 'Welcome!'
+		}
 
+
+	def add():
+		name = input('name: ')
+		url_name = name.replace(' ', '-').lower()
+
+		try:
+			site = requests.get(data['site'] + url_name)
+
+			if site.ok:
+				print('Done!')
+				_add_to_data(name, site)
+			else:
+				print('Invalid.')
+				exit()
+		except Exception as e:
+			print('An error occurred, bip bop:', e)
+			exit()
+
+
+	def remove():
+		name = input('name: ')
+
+		try:
+			if name in data['animes']:
+				data['animes'].pop(name)
+				print('Done!')
+		except Exception as e:
+			print('An error occurred, bip bop:', e)
+			exit()
+
+		with open('data.json', 'w') as f:
+			json.dump(data, f, sort_keys=False, indent=4)
+			f.close()
+
+
+	def update():
+		new = False
+
+		for anime in data['animes']:
+			anime_id = data['animes'][anime][0]
+
+			try:
+				download_page = requests.get(data['prefix'] + anime_id)
+				latest_release = re.search('id="(\d+?)"', str(download_page.content)).group(1)
+			except Exception as e:
+				print('An error occurred, bip bop:', e)
+
+			if data['animes'][anime][1] < int(latest_release):
+				new = True
+
+				print(f'A new episode of {anime} has been released! EP {int(latest_release)}.')
+
+				data['animes'][anime] = [anime_id, int(latest_release)]
+
+				with open('data.json', 'w') as f:
+					json.dump(data, f, sort_keys=False, indent=4)
+					f.close()
+
+				quality = data['quality']
+				url = re.search(f'<div class="rls-link link-{quality}p" id="{latest_release}-{quality}p">.*?<a title="Torrent Link" href="(.*?)".*?<\/div>', str(download_page.content)).group(1)
+				print(f'Download the episode here: {url}\n')
+
+		if new == False:
+			print('There are no new releases. Unlucky.')
+
+
+	def quality(arg):
+		if arg == '480':
+			data['quality'] = arg
+			print(f'Default quality set to {arg}p.')
+		elif arg == '720':
+			data['quality'] = arg
+			print(f'Default quality set to {arg}p.')
+		elif arg == '1080':
+			data['quality'] = arg
+			print(f'Default quality set to {arg}p.')
+		else:
+			print('Invalid argument.')
+			exit()
+
+		with open('data.json', 'w') as f:
+			json.dump(data, f, sort_keys=False, indent=4)
+			f.close()
+
+
+	def list():
+		for anime in data['animes']:
+			ep = data['animes'][anime][1]
+			print(f'{anime}. EP {ep}.')
+
+
+def _add_to_data(name, site):
+	anime_id = re.findall('hs_showid = \d+', str(site.content))[0].replace(' ', '').split('=')[1]
+
+	download_page = requests.get(data['prefix'] + anime_id)
+	latest_release = re.search('id="(\d+?)"', str(download_page.content)).group(1)
+	
+	data['animes'][name] = [anime_id, int(latest_release)]
 	with open('data.json', 'w') as f:
 		json.dump(data, f, sort_keys=False, indent=4)
-else:
-    print('Ainda não saiu ep novo de Kimetsu no Yaiba.') 
+		f.close()
 
 
-if opm.status_code == 200:
-	print(f'Saiu ep novo de One Punch Man! EP {opm_id}.')
-	data['opm'] += 1
 
-	with open('data.json', 'w') as f:
-		json.dump(data, f, sort_keys=False, indent=4)
-else:
-	print('Ainda não saiu ep novo de One Punch Man.')
-
-
-if dororo.status_code == 200:
-	print(f'Saiu ep novo de Dororo! EP {dororo_id}.')
-	data['dororo'] += 1
-
-	with open('data.json', 'w') as f:
-		json.dump(data, f, sort_keys=False, indent=4)
-else:
-	print('Ainda não saiu ep novo de Dororo.')
+if __name__ == '__main__':
+	Commands()
---
+++ parse.py
@@ -0,0 +1,62 @@
+import inspect
+import sys
+import re
+
+
+class Parse:
+	def __init__(self, child):
+		self.child = child
+		self._get_description()
+		self._exec()
+
+
+	def _get_description(self):
+		self.methods = list(filter(lambda method: not re.search('^_', method), dir(self.child)))
+
+		self.description = dict()
+
+		for i in range(len(self.methods)):
+			self.description[self.methods[i]] = getattr(self.child, self.methods[i]).__doc__
+
+
+	def _exec(self):
+		try:
+			if len(sys.argv) < 2:
+				if self._greeting:
+					print(self._greeting)
+		except Exception:
+			pass
+
+		try:
+			if sys.argv[1] == 'version':
+				if self._version:
+					print(self._version)
+		except Exception:
+			pass
+
+		try:
+			if sys.argv[1] == 'help':
+				for key, value in self.description.items():
+					print(f'{key}: {value}')
+		except Exception:
+			pass
+
+		try:
+			func = getattr(self.child, sys.argv[1])
+			params = inspect.getfullargspec(func).args
+			args = sys.argv[2:]
+
+			if len(params) == len(args):
+				func(*args)
+		except Exception:
+			pass
+
+
+def set_root(function):
+	try:
+		Parse._version = function()['version']
+		Parse._greeting = function()['greeting']
+	except KeyError as e:
+		Parse._version = False
+		Parse._greeting = False
+		print('Error:', e)
