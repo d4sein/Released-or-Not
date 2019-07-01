1856ce6fd4b0ebb0b807399fb4abab994458ad9d
Willian Nascimento <45379088+d4sein@users.noreply.github.com>
2019-07-01 20:23:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from parse import *
+from argila import *
+from bs4 import BeautifulSoup
 import colorama
 import requests
 import json
@@ -13,132 +14,139 @@ with open('data.json', 'r') as f:
 	data = json.load(f)
 
 
-class Commands(Parse):
+class Commands(Argila):
 	def __init__(self):
 		super().__init__(Commands)
 
-
-	@set_root
+	@root
 	def _root():
 		return {
-		'application': 'ron',
-		'version': 'Version 1.0.0',
+		'head': 'Script that uses web scraping to track anime releases',
+		'version': 'Version 0.1.0',
 		'greeting': 'Welcome!',
-		'head': '''Released or Not v1.0.0
-Willian Nascimento \u001b[38;5;3m<williann.nasc@gmail.com>\u001b[0m
-Script that uses web scraping to track anime releases'''
+		'call': 'main.py',
 		}
 
+	def add() -> 'add':
+		'''Adds an Anime to the list'''
+		anime = input('name: ')
 
-	def add():
-		'''Adds a new Anime to your list'''
-		name = input('name: ')
-		
-		if name in data['animes']:
-			print('You\'re already following this Anime.')
-			exit()
-
-		url_name = name.replace(' ', '-').lower()
+		sufix = anime.replace(' ', '-').lower()
 
 		try:
-			site = requests.get(data['site'] + url_name)
-
-			if site.ok:
-				_add_to_data(name, site)
-			else:
-				print('Invalid.')
-				exit()
-		except Exception as e:
-			print('An error occurred, bip bop.')
+			site = requests.get(data['site'] + sufix)
+		except Exception:
+			print('An error has occurred, bip bop.')
 			exit()
 
+		if site.ok:
+			soup = BeautifulSoup(site.content, 'html.parser')
+			title = soup.select_one('title').text.split(' – ')[0]
+		else:
+			print('Invalid anime title or couldn\'t be found.')
+			exit()
 
-	def remove():
-		'''Removes an Anime from your list'''
-		name = input('name: ')
+		if title in data['animes']:
+			print('You\'re already following this anime, bop.')
+			exit()
+		else:
+			_add_to_data(title, site)
+
+	def remove() -> 'remove':
+		'''Removes an Anime from the list'''
+		anime = input('name: ')
 
 		try:
-			if name in data['animes']:
-				data['animes'].pop(name)
+			if anime in data['animes']:
+				data['animes'].pop(anime)
 				print('Done!')
 			else:
-				print('This Anime is not in your list.')
+				print('You\'re not following this anime.')
 				exit()
 		except Exception:
-			print('An error occurred, bip bop.')
+			print('An error has occurred, bip bop.')
 			exit()
 
 		with open('data.json', 'w') as f:
 			json.dump(data, f, sort_keys=False, indent=4)
 			f.close()
 
-
-	def update():
-		'''Checks if a new episode has been released'''
+	def update() -> 'update':
+		'''Checks if new episodes have been released'''
 		new = False
 
-		for anime in data['animes']:
-			anime_id = data['animes'][anime][0]
+		for title in data['animes']:
+			animeID = data['animes'][title][0]
 
 			try:
-				download_page = requests.get(data['prefix'] + anime_id)
+				download_page = requests.get(data['prefix'] + animeID)
 				latest_release = re.search('id="(\d+?)"', str(download_page.content)).group(1)
-			except Exception as e:
-				print('An error occurred, bip bop:', e)
+			except Exception:
+				print('An error has occurred, bip bop.')
 
-			if data['animes'][anime][1] < int(latest_release):
-				new = True
+			if data['animes'][title][1] < int(latest_release):
+				print(f'A new episode of {title} has been released! EP {int(latest_release)}.')
 
-				print(f'A new episode of {anime} has been released! EP {int(latest_release)}.')
-
-				data['animes'][anime] = [anime_id, int(latest_release)]
+				kv = {
+					title: [
+						animeID,
+						int(latest_release)
+					]
+				}
+				
+				data['animes'].update(kv)
 
 				with open('data.json', 'w') as f:
 					json.dump(data, f, sort_keys=False, indent=4)
 					f.close()
 
 		if new == False:
-			print('There are no new releases. Unlucky.')
+			print('There are no new releases. bip.')
 
+	def list() -> 'list':
+		'''Shows list of Animes being followed'''
+		for title in data['animes']:
+			ep = data['animes'][title][1]
+			print(f'\u001b[38;5;3m>\u001b[0m {title} [\u001b[38;5;3mEP {ep}\u001b[0m]')
 
-	def list():
-		'''Shows full list of Animes being followed'''
-		for anime in data['animes']:
-			ep = data['animes'][anime][1]
-			print(f'\u001b[38;5;3m>\u001b[0m {anime} [\u001b[38;5;3mEP {ep}\u001b[0m]')
-
-
-	def erase():
-		'''Erases your list completely (no recovery)'''
+	def erase() -> 'erase':
+		'''Erases the list completely'''
 		try:
-			validation = input('Are you sure you want to erase your list? After that, you won\'t be able to recover it. [\u001b[38;5;3my\u001b[0m/\u001b[38;5;3mn\u001b[0m]\n>')
+			validation = input('Are you sure you want to erase your list? After that, you won\'t be able to recover it. [\u001b[38;5;3my\u001b[0m/\u001b[38;5;3mn\u001b[0m]\n')
 		except Exception:
 			pass
 
 		if validation == 'y':
 			data['animes'] = {}	
+			
 			with open('data.json', 'w') as f:
 				json.dump(data, f, sort_keys=False, indent=4)
 				f.close()
+
 			print('Done!')
 		else:
 			exit()
 
-
-def _add_to_data(name, site):
-	anime_id = re.findall('hs_showid = \d+', str(site.content))[0].replace(' ', '').split('=')[1]
-
-	download_page = requests.get(data['prefix'] + anime_id)
+def _add_to_data(title, site):
+	animeID = re.search('hs_showid = (\d+)', str(site.content)).group(1)
+	download_page = requests.get(data['prefix'] + animeID)
 	latest_release = re.search('id="(\d+?)"', str(download_page.content)).group(1)
 	
-	data['animes'][name] = [anime_id, int(latest_release)]
+	kv = {
+		title: [
+			animeID,
+			int(latest_release)
+		]
+	}
+
+	data['animes'].update(kv)
+
 	with open('data.json', 'w') as f:
 		json.dump(data, f, sort_keys=False, indent=4)
 		f.close()
 
 	print('Done!')
 
-
-
 if __name__ == '__main__':
+	sys.argv = ['main.py', '-e']
 	Commands()
